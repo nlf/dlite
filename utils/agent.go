5fd0632b0f87a64044625e227809ad4d82b0308e
Uwe Dauernheim <uwe@dauernheim.net>
2016-02-19 10:20:51 +0000

    Unify using tabs

--- utils/agent.go
+++ utils/agent.go
@@ -11,18 +11,18 @@ import (
 const template = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
-  <dict>
-	  <key>Label</key>
+	<dict>
+		<key>Label</key>
 		<string>local.dlite</string>
 		<key>ProgramArguments</key>
 		<array>
-		  <string>/usr/bin/sudo</string>
-		  <string>%s</string>
+			<string>/usr/bin/sudo</string>
+			<string>%s</string>
 			<string>daemon</string>
 		</array>
 		<key>RunAtLoad</key>
 		<true/>
-  </dict>
+	</dict>
 </plist>
 `
 
